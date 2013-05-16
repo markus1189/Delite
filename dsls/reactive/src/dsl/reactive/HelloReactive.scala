aa4b130812576cd42774a41841ee65dd210b760f
Markus Hauck <markus1189@gmail.com>
2013-05-16 13:57:03 +0200

    Cleanup hello reactive

--- dsls/reactive/src/dsl/reactive/HelloReactive.scala
+++ dsls/reactive/src/dsl/reactive/HelloReactive.scala
@@ -5,9 +5,10 @@ object HelloReactiveRunner extends ReactiveApplicationRunner with HelloReactive
 trait HelloReactive extends ReactiveApplication { 
   def main() = {
     val x = ReactiveVar(5)
-    val y = x.get
     println(x.get)
     x.set(42)
+    println(x.get)
+    x.set(1337)
     println(x.get)
   }
 }
