ac72528e81084f0fbdbcfea4e33c647f5826b085
Markus Hauck <markus1189@gmail.com>
2013-06-14 15:24:44 +0200

    Add another layer to TestChaining

--- dsls/reactive/src/dsl/reactive/TestChaining.scala
+++ dsls/reactive/src/dsl/reactive/TestChaining.scala
@@ -10,11 +10,13 @@ trait TestChaining extends ReactiveApplication {
     val e1 = Signal(root) { root.get + 1 }
     val e2 = Signal(e1) { e1.get + 1 }
     val e3 = Signal(e2) { e2.get + 1 }
+    val e4 = Signal(e3) { e3.get + 1 }
 
-    println(root.get + ", " + e1.get + ", " + e2.get + ", " + e3.get)
-
+    println("0: " + root.get + ", " + e1.get + ", " + e2.get + ", " + e3.get + ", " + e4.get)
     root.set(2)
-    println(root.get + ", " + e1.get + ", " + e2.get + ", " + e3.get)
+    println("1: " + root.get + ", " + e1.get + ", " + e2.get + ", " + e3.get + ", " + e4.get)
+    root.set(3)
+    println("2: " + root.get + ", " + e1.get + ", " + e2.get + ", " + e3.get + ", " + e4.get)
   }
 }
 
