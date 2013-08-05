6f99265895f46fb0d35b6e4deded6ee3f94a4464
Markus Hauck <markus1189@gmail.com>
2013-08-05 14:43:28 +0200

    Use inferred signals in constant conversion

--- dsls/reactive/src/dsl/reactive/case_testing/ConstantConversion.scala
+++ dsls/reactive/src/dsl/reactive/case_testing/ConstantConversion.scala
@@ -4,9 +4,9 @@ object ConvertConstantsRunner extends ReactiveApplicationRunner with ConvertCons
 
 trait ConvertConstants extends ReactiveApplication {
   def main() = {
-    val c1 = Signal() { 42 }
-    val c2 = Signal() { 1337 }
-    val shouldBecomeConstant = Signal (c1,c2) { c1.get + c2.get }
+    val c1 = ISignal { 42 }
+    val c2 = ISignal { 1337 }
+    val shouldBecomeConstant = ISignal { c1.get + c2.get }
 
     println(shouldBecomeConstant.get)
   }
